f3195b4fb91b6a27feed00264cba09ce550f047f
Pashimati <pashimatii@gmail.com>
2021-11-23 21:46:06 +0300

    cart

---
+++ js/cart.js
@@ -0,0 +1,17 @@
+const cart = function () {
+    const cartBtn = document.querySelector('.button-cart')
+    const cart = document.querySelector('#modal-cart')
+    const closeBtn = cart.querySelector('.modal-close')
+
+    console.log(cart)
+
+    cartBtn.addEventListener("click", () => {
+        cart.style.display = 'flex'
+    })
+
+    closeBtn.addEventListener("click", () => {
+        cart.style.display = ''
+    })
+}
+
+cart()
