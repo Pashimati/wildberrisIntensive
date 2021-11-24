f323ff6deab588567cecd7489fc67523aafa6854
Pashimati <pashimatii@gmail.com>
2021-11-24 22:17:23 +0300

    filter and search

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,19 +1,63 @@
 const getGoods = () => {
     const links = document.querySelectorAll('.navigation-link')
 
-    const getData = () => {
+    const renderGoods = (goods) => {
+
+        const goodContainer = document.querySelector('.long-goods-list')
+
+        goodContainer.innerHTML = ''
+
+        goods.forEach(good => {
+            const goodBlock = document.createElement('div')
+
+            goodBlock.classList.add('col-lg-3')
+            goodBlock.classList.add('col-sm-6')
+
+            goodBlock.innerHTML = `
+                <div class="goods-card">
+                    <span class="label ${good.label ? null : 'd-none'}">${good.label}</span>
+                    <img src="/wildberrisIntensive/db/${good.img}" alt="${good.name}" class="goods-image">
+                    <h3 class="goods-title">${good.name}</h3>
+                    <p class="goods-description">${good.description}</p>
+                    <button class="button goods-card-btn add-to-cart" data-id="${good.id}">
+                        <span class="button-price">$${good.price}</span>
+                    </button>
+                </div>
+            `
+            goodContainer.append(goodBlock)
+
+        })
+    }
+
+    const getData = (value, category) => {
        fetch('/db/db.json')
            .then((res) => res.json())
            .then((data) => {
-               localStorage.setItem('goods', JSON.stringify(data))
+               const array = category ? data.filter((item) => item[category] === value) : data
+
+               localStorage.setItem('goods', JSON.stringify(array))
+
+               if (window.location.pathname !== "/wildberrisIntensive/goods.html") {
+                   window.location.href = "/wildberrisIntensive/goods.html"
+               } else {
+                    renderGoods(array)
+                }
            })
    }
     links.forEach((link) => {
         link.addEventListener('click', (event) => {
             event.preventDefault()
-            getData()
+            const linkValue = link.textContent
+            const category = link.dataset.field
+
+            getData(linkValue, category)
         })
     })
+
+    if (localStorage.getItem('goods') && window.location.pathname === "/wildberrisIntensive/goods.html") {
+        renderGoods(JSON.parse(localStorage.getItem('goods')))
+    }
+
 }
 
 getGoods()
